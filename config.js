ade7f7dcaaf6d7e09d07dceffe0d5d1414c4f441
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-28 15:25:25 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -5,15 +5,17 @@
  */
 window.CARETAXI_CONFIG = Object.freeze({
   APP_NAME: "DPRO 介護タクシー LINE",
-  VERSION: "CARETAXI-6-OWNER-IPAD-20260728",
+  VERSION: "CARETAXI-7-STAFF-OPERATIONS-20260728",
   API_BASE_URL: "https://dpro-caretaxi-line-api.dpromstk2000.workers.dev",
   ORGANIZATION_CODE: "dpro_caretaxi_demo",
   LIFF_ID: "",
   DEMO_MODE: true,
+  BUSINESS_PHONE: "092-000-9999",
   REQUEST_TIMEOUT_MS: 15000,
   DRAFT_STORAGE_KEY: "dpro_caretaxi_request_draft_v2",
   DEMO_LINE_USER_KEY: "dpro_caretaxi_demo_line_user_v1",
   MEMBER_SESSION_KEY: "dpro_caretaxi_member_session_v1",
   ADMIN_SESSION_KEY: "dpro_caretaxi_admin_session_v1",
+  STAFF_SESSION_KEY: "dpro_caretaxi_staff_session_v1",
   REPEAT_PREFILL_KEY: "dpro_caretaxi_repeat_prefill_v1"
 });

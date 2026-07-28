37ead965c857b27ed5233a90aa38da79e0060707
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-28 11:24:04 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+/**
+ * DPRO 介護タクシー LINE
+ * STEP CARETAXI-3 公開設定
+ * 秘密鍵・Service Role Key・LINE Channel Secretは絶対に記載しないこと。
+ */
+window.CARETAXI_CONFIG = Object.freeze({
+  APP_NAME: "DPRO 介護タクシー LINE",
+  VERSION: "CARETAXI-3-LINE-REQUEST-20260728",
+  API_BASE_URL: "https://dpro-caretaxi-line-api.dpromstk2000.workers.dev",
+  ORGANIZATION_CODE: "dpro_caretaxi_demo",
+  LIFF_ID: "",
+  DEMO_MODE: true,
+  REQUEST_TIMEOUT_MS: 15000,
+  DRAFT_STORAGE_KEY: "dpro_caretaxi_request_draft_v1",
+  DEMO_LINE_USER_KEY: "dpro_caretaxi_demo_line_user_v1"
+});
